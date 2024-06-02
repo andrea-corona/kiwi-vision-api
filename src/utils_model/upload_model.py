fa5128e8c50defddc007a81330f7576ff779767c
andrea-corona <coronaarroyo@hotmail.com>
2024-06-01 22:10:07 -0600

    updates

--- src/utils_model/upload_model.py
+++ src/utils_model/upload_model.py
@@ -2,6 +2,7 @@ import sys
 sys.path.append("..")
 import onnxruntime as ort
 from decouple import config
+import boto3
 import json
 # Load classes from the JSON file
 with open('./src/assets/classes.json', 'r') as file:
@@ -11,7 +12,11 @@ CLASSES = classes_json['classes']
 
 class Model:
     def __init__(self):
+        s3_client = boto3.client('s3', aws_access_key_id=config('access-key'), aws_secret_access_key=config('secret-access-key'))
+        response = s3_client.get_object(Bucket=config('bucket'), Key=config('model'))
+        object_content = response['Body'].read()
         providers = ['CUDAExecutionProvider', 'CPUExecutionProvider'] if config('device') == 'cuda' else ['CPUExecutionProvider']
-        self.model = ort.InferenceSession(config('model'), providers=providers)
+        self.model = ort.InferenceSession(object_content, providers=providers)
+
 
 session = Model()
